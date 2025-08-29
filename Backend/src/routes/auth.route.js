4e77a01207ddf13b8d67d1b704f1fa56b002a79c
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-30 00:40:27 +0530

    Backend auth halfway done

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { signup, login , logout } from '../controllers/auth.controller.js'
+import { signup, login , logout, updateProfile, checkAuth } from '../controllers/auth.controller.js'
 import { protectRouter } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
@@ -11,5 +11,7 @@ router.post("/login",login)
 router.post("/logout",logout)
 
 router.put("/update-profile",protectRouter ,updateProfile)
+
+router.get("/check",protectRouter,checkAuth)
 
 export default router
